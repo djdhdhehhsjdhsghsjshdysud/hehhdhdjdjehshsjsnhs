a0f918a1e71807a8d3b21653947bd9a4122da0b2
djdhdhehhsjdhsghsjshdysud <107333239+djdhdhehhsjdhsghsjshdysud@users.noreply.github.com>
2022-06-12 14:02:25 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 2164808
 api_hash = "2af46b76e38461db8b0b078dfa79c2a8"
-bot_token = "5274949594:AAExfSc0FdY0BerCnu0wFTdAL0DdJeiqQnk"
+bot_token = "5274949594:AAH2RJ7dVK2azQb-Sei2qzZ4c4jUvMq5Hm8"
 
